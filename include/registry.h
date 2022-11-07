3514486f6f20d6eb1e5f56883ad257aa02ce37da
lebed <lebed@DESKTOP-BKIICKL>
2022-11-07 19:32:36 +0300

    Add project files.

---
+++ include/registry.h
@@ -0,0 +1,91 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <map>
+
+
+template <typename T>
+T enterData()
+{
+    T value;
+    std::cin >> value;
+
+    return value;
+}
+
+template <typename Key, typename Value>
+
+class Registry
+{
+public:
+    void add()
+    {
+        std::cout << "Enter key: ";
+        Key key = enterData<Key>();
+        std::cout << "Enter value: ";
+        Value value = enterData<Value>();
+
+        system("cls");
+
+        registry.insert(std::make_pair(key, value));
+        std::cout << "Key: " << key << std::endl;
+        std::cout << "Value: " << value << std::endl;
+        std::cout << "Was added on registry" << std::endl;
+        std::cout << "          " << std::endl;
+    }
+
+    void remove()
+    {
+        std::cout << "Enter key: ";
+        Key key = enterData<Key>();
+
+        system("cls");
+
+        auto itf = registry.find(key);
+        if (itf != registry.end())
+        {
+            registry.erase(key);
+            std::cout << "Key: " << key << " was deleted " << std::endl;
+            std::cout << "          " << std::endl;
+        }
+        else
+        {
+            std::cout << "Key: " << key << " doesn't find " << std::endl;
+            std::cout << "          " << std::endl;
+        }
+    }
+
+    void print()
+    {
+        system("cls");
+
+        for (const auto& key : registry)
+        {
+            std::cout << "Key: " << key.first << '\t';
+            std::cout << "Value: " << key.second << std::endl;
+        }
+    }
+
+    void find()
+    {
+        std::cout << "Enter key: ";
+        Key key = enterData<Key>();
+
+        system("cls");
+
+        auto itf = registry.equal_range(key);
+
+        std::cout << "Key: " << key << std::endl;
+        std::cout << "Value: ";
+        
+        while (itf.first != itf.second)
+        {
+            std::cout << itf.first->second << ' ';
+            ++itf.first;
+        }
+        std::cout << "          " << std::endl;
+    }
+
+private:
+    std::multimap <Key, Value> registry;
+};
---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include "registry.h"
+
+
+
+int main()
+{
+    Registry<int, std::string> myRegistry;
+    std::string commands[5]{ "add", "remove", "print", "find", "exit" };
+
+    bool exit = false;
+    while (!exit)
+    {
+        
+        
+        std::string command;
+        bool flag = false;
+        while (!flag)
+        {
+            std::cout << "          " << std::endl;
+            std::cout << "1. add;" << std::endl;
+            std::cout << "2. remove;" << std::endl;
+            std::cout << "3. print;" << std::endl;
+            std::cout << "4. find;" << std::endl;
+            std::cout << "5. exit." << std::endl << std::endl;
+            std::cout << "Enter command: " << std::endl;
+            std::cin >> command;
+            for (int i = 0; i < 5; ++i)
+            {
+                if (command == commands[i])
+                {
+                    break;
+                }
+                else
+                {
+                    flag = true;
+                    system("cls");
+                    break;
+                }
+            }
+            if (command == "add")
+            {
+                myRegistry.add();     
+            }
+            else if (command == "remove")
+            {
+                myRegistry.remove();   
+            }
+            else if (command == "print")
+            {
+                myRegistry.print();
+            }
+            else if (command == "find")
+            {
+                myRegistry.find();
+            }
+            else if (command == "exit")
+            {
+                exit = true;
+            }
+        }     
+    }
+}
